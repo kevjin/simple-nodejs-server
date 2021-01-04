a7aaf8d25e1f44ebf941604ddf65250dd7012d08
Kevin Jin <kevin.jin@vanderbilt.edu>
2021-01-04 14:36:14 -0700

    test

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
+// test
 // Log all requests
 app.use((req, res, next) => {
   console.log("Request query parameters:");
